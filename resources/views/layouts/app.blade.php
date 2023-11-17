173919a62aeafde51e442ec826e74c6c7e8042ca
LEN-RPL01 student <1rismaauliaputri@gmail.com>
2023-11-17 13:22:23 +0700

    mau presentasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,63 +18,53 @@
     <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css">
     <link rel="icon" href="{{asset('process.png')}}">
 
-    <style>
-        .navbar .nav-item .nav-link:hover {
-            background-image: linear-gradient(to right, gold, yellow);
-    -webkit-background-clip: text;
-    background-clip: text;
-    color: transparent; 
-        }
-    </style>
-
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body class="bg-simple">
-<div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-            <a class="navbar-brand  text-warning fw-bold d-flex align-items-center " href="{{ url('/dashboard') }} ">
-                <!-- <img src="{{asset('mail.png')}}" class="w-25"> -->
-                <h2 class="fw-bold m-0 text-gradient" style="">
-                   sikad bang
-                </h2>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar Example</title>
+    <style>
+       
+        body {
+            font-family: Arial, sans-serif;
+            margin: 10;
+            padding: 10;
+        }
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto align-items-center">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item text-gradient">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
+        .navbar {
+            background-color: #C5C5F1;
+            overflow: hidden;
+        }
 
-                    @else
-                        <li class="nav-item mx-4 d-flex align-items-center">
-                            <h1 class="m-auto mx-2 text-gradient"><i class="bi bi-person"></i></h1>
-                            <div>
-                                <p class="mb-0 fs-5 text-gradient" >{{Auth::user()->username}}</p>
-                                <p class="text-capitalize m-0 text-white">{{Auth::user()->role}}</p>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="btn logout btn-danger" href="{{ route('logout') }}">{{ __('Logout') }}</a>
-                        </li>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
+        .navbar a {
+            float: left;
+            display: block;
+            color: black;
+            text-align: center;
+            padding: 5px 3px;
+            text-decoration: none;
+            font-size: 20px;
+        }
 
+        .navbar a:hover {
+            background-color: #A0A0F3;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <a href="#user">User</a>
+        <a href="#pemasukan">Pemasukan</a>
+        <a href="{{ route('pengeluaran.create') }}">Pengeluaran</a>
+        <a href="#logs">Logs</a>
+    </div>
+</body>
+</html>
     <main class="py-4 container">
         @include('layouts.flash-message')
         @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PengeluaranKasController;
 use App\Http\Controllers\TblUserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +16,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('', function() {
-    return redirect('/login');
+    return redirect('/pengeluaran');
 });
 
 Route::get('/login', [TblUserController::class, 'index'])->name('login');
 Route::post('/login', [TblUserController::class, 'login']);
 Route::get('/logout', [TblUserController::class, 'logout'])->name('logout');
+
+// Menampilkan semua data kas keluar
+Route::get('/pengeluaran', [PengeluaranKasController::class, 'index'])->name('pengeluaran.index');
+
+// Menampilkan formulir untuk membuat data kas keluar baru
+Route::get('/pengeluaran/create', [PengeluaranKasController::class, 'create'])->name('pengeluaran.create');
+
+// Menyimpan data kas keluar yang baru dibuat
+Route::post('/pengeluaran', [PengeluaranKasController::class, 'store'])->name('pengeluaran.store');
+
+// Menampilkan detail dari sebuah data kas keluar
+Route::get('/pengeluaran/{id}', [PengeluaranKasController::class, 'show'])->name('pengeluaran.show');
+
+// Menampilkan formulir untuk mengedit data kas keluar
+Route::get('/pengeluaran/{id}/edit', [PengeluaranKasController::class, 'edit'])->name('pengeluaran.edit');
+
+// Menyimpan perubahan pada data kas keluar yang sudah diedit
+Route::put('/pengeluaran/{id}', [PengeluaranKasController::class, 'update'])->name('pengeluaran.update');
+
+// Menghapus data kas keluar
+Route::delete('/pengeluaran/{id}', [PengeluaranKasController::class, 'destroy'])->name('pengeluaran.destroy');
